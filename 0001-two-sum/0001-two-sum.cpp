828877fbc5b199d4b0077e7c7e9b1cd7b2aed011
Apurva Pingale <93487953+Apurvapingale@users.noreply.github.com>
2023-09-05 16:15:48 +0530

    Time: 3 ms (99.40%), Space: 10.8 MB (45.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // for(int i =0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return{i,j};
+        //         }
+        //     }
+        // }
+        // return {};
+        //efficient
+        vector <int> ans;
+        unordered_map<int,int> mpp;
         for(int i =0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return{i,j};
-                }
+            if(mpp.find(target-nums[i])!=mpp.end()){
+                //map
+                ans.push_back(mpp[target-nums[i]]);
+                //cureebt index
+                ans.push_back(i);
+                return ans;
+                
             }
+            //if not store the current nums of i in the hash map
+            mpp[nums[i]]=i;
         }
-        return {};
+        return ans;
     }
 };
